1563aa9453fd63e8b4d35343a4d19af45c314254
shravan090 <31248954+shravan090@users.noreply.github.com>
2018-07-05 14:04:50 +0530

    Add files via upload

---
+++ Week1/Day5/fibonacci.py
@@ -0,0 +1,76 @@
+import unittest
+
+
+
+def fib(n):
+    # Compute the nth Fibonacci number
+    a=0
+    b=1
+    if n<0:
+        raise Exception
+    if n==0:
+        return a
+    elif n==1:
+        return 1
+    else:
+        c=0
+        for i in range(2,n+1):
+            c=a+b
+            a=b
+            b=c
+        return c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_zeroth_fibonacci(self):
+        actual = fib(0)
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_first_fibonacci(self):
+        actual = fib(1)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_second_fibonacci(self):
+        actual = fib(2)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_third_fibonacci(self):
+        actual = fib(3)
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_fifth_fibonacci(self):
+        actual = fib(5)
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_tenth_fibonacci(self):
+        actual = fib(10)
+        expected = 55
+        self.assertEqual(actual, expected)
+
+    def test_negative_fibonacci(self):
+        with self.assertRaises(Exception):
+            fib(-1)
+
+
+unittest.main(verbosity=2)
